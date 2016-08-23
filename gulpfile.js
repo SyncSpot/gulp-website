f7ba7d65d1f817f122590c39b34cce189eea6f7d
mjdude <mo@syncspot.net>
2016-08-23 20:07:39 +0100

    initial commit with gulp file template

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+
+// Styles
+gulp.task('styles', function(){
+  console.log('Starting styles task');
+});
+
+// Scripts
+gulp.task('scripts', function(){
+  console.log('Starting scripts tasks');
+});
+
+// Images
+gulp.task('images', function(){
+  console.log('Starting images task');
+});
+
+gulp.task('default', function(){
+  console.log('Starting default task');
+});
